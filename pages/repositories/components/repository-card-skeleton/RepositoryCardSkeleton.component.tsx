28a9f5b577cef272bed81634edc3646a24901f66
Ivan Escribano <ivanescribano1998@gmail.com>
2023-11-23 13:09:50 +0100

    height skeleton

--- pages/repositories/components/repository-card-skeleton/RepositoryCardSkeleton.component.tsx
+++ pages/repositories/components/repository-card-skeleton/RepositoryCardSkeleton.component.tsx
@@ -3,13 +3,12 @@ import React from 'react';
 import { Skeleton } from '@mui/material';
 
 import { RepositoryCardSkeletonProps } from './RepositoryCardSkeleton.interface';
-import styles from './RepositoryCardSkeleton.module.scss';
 
 const RepositoryCardSkeleton = ({ skeletonsNumber }: RepositoryCardSkeletonProps) => {
   return (
     <>
       {Array.from(new Array(skeletonsNumber)).map((_, index) => (
-        <Skeleton key={index} animation={'pulse'} variant="rounded" className={styles.skeleton} role="img" height={'300'} />
+        <Skeleton key={index} animation={'pulse'} variant="rounded" role="img" height={'300px'} />
       ))}
     </>
   );
