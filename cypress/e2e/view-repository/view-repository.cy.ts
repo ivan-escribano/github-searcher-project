f8d209e1ce0d423dd4c69d44ff63e87c239bf3a3
Ivan Escribano <ivanescribano1998@gmail.com>
2023-11-30 20:44:59 +0100

    feat(cypress): end to end testing view a specific repository

--- cypress/e2e/view-repository/view-repository.cy.ts
+++ cypress/e2e/view-repository/view-repository.cy.ts
@@ -4,10 +4,16 @@ describe('Check flow searching for a github repository', () => {
   it('Should search for a repository and go to the corresponding repository', () => {
     cy.visit('http://localhost:3000');
 
+    cy.window().then((win) => {
+      cy.stub(win, 'open').as('windowOpen');
+    });
+
     const inputSearcher = cy.get('[data-cy="input-searcher"]', { timeout: 10000 }).should('be.visible');
     inputSearcher.type('github-searcher-project');
 
     const buttonAction = cy.get('[data-cy="button-action"]').first();
     buttonAction.click();
+
+    cy.get('@windowOpen').should('have.been.calledWith', 'https://github.com/ivan-escribano/github-searcher-project');
   });
 });
